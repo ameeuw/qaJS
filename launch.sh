7998cad56de2fc5c88d776b16902f94eb12cf008
Arne Meeuw <arne.meeuw@gmail.com>
2017-05-17 23:00:47 +0200

    Backgrounding in launch script enabled

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 if [ $1 = "pyserver" ]; then
   echo "Starting python server"
-  /usr/bin/python /home/pi/qaJS/python/server.py
+  /usr/bin/python /home/pi/qaJS/python/server.py &
 else
   echo "Starting nodejs server"
   /usr/local/bin/node /home/pi/qaJS/node/server.js dht co2 &
